a6001e2bfb3086bfdc0e5e3884f9088d62146425
“hhy” <1425301899@qq.com>
2021-06-18 20:33:36 +0800

    first commit

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void loginSuccess(){
+        System.out.println("登录成功");
+    }
+}
---
+++ src/test/java/testcases/RegisterTest.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class RegisterTest {
+    @Test
+    public void registerSuccuess(){
+        System.out.println("注册成功");
+    }
+}
